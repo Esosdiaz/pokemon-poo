24e167b46388e92364c627877bb18f1f80c79064
Fa Diaz <diazgonzalez.perla@gmail.com>
2017-05-04 21:34:07 -0400

    batalla pokemon e imagenes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,7 +6,7 @@ function Pokemon(nombre, color, poderDeAtaque){
 	this.vida = 100
 	this.poderDeAtaque = poderDeAtaque
 	this.mostrarPokemon = function(){
-		document.getElementById("parrafo").innerHTML += ("hola, soy " + this.nombre + " y soy de color " + this.color + "<br>");
+		document.getElementById("parrafo").innerHTML += ("Hola, soy " + this.nombre + " y soy de color " + this.color + "<br>");
 	}
 	this.aumentarAmistad = function(valor){
 		this.nivelDeAmistad = this.nivelDeAmistad + valor;
@@ -14,7 +14,7 @@ function Pokemon(nombre, color, poderDeAtaque){
 	this.atacar = function(pokemon){
 		pokemon.vida = pokemon.vida - this.poderDeAtaque;
 		var pelea = document.getElementById("pelea")
-		pelea.innerHTML = this.nombre + " atacó a " + pokemon.nombre + " y este ahora tiene " + pokemon.vida + " vida restante.";
+		pelea.innerHTML = this.nombre + " atacó a " + pokemon.nombre + " y este ahora tiene " + pokemon.vida + " vida restante. <br><br>";
 	}
 }
 
@@ -24,7 +24,18 @@ function pelear(){
 	var numRandom = Math.floor((Math.random() * 100) + 1);
 	var atacante = new Pokemon(pokemon1, "amarillo", numRandom);
 	var atacado = new Pokemon(pokemon2, "rojo", numRandom);
+	if (atacante.nombre == atacado.nombre){
+		alert("sean amiguitos!")
+	} else {
 	atacante.atacar(atacado); 
-
+	}
 }
 
+const Pikachu = new Pokemon("Pikachu", "amarillo", 100)
+Pikachu.mostrarPokemon();
+const Charmander = new Pokemon("Charmander", "naranjo", 100)
+Charmander.mostrarPokemon();
+const Squirtle = new Pokemon("Squirtle", "celeste", 100)
+Squirtle.mostrarPokemon();
+const Bulbasaur = new Pokemon("Bulbasaur", "verde", 100)
+Bulbasaur.mostrarPokemon();
